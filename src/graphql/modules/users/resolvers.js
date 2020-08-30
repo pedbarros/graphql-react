1daf9c87714c01fd220be46c5ccae88b75e9ed91
Pedro Barros <pedro.barros@herospark.com>
2020-08-30 19:07:37 -0300

    Create subscription to listening user added

--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -1,4 +1,5 @@
 import User from '../../../models/User';
+import { USER_ADDED } from './channels';
 
 export default {
   User: {
@@ -9,11 +10,24 @@ export default {
     user: (_, { id }) => User.findById(id),
   },
   Mutation: {
-    createUser: (_, { data }) => User.create(data),
+    createUser: async (_, { data }, { pubsub }) => {
+      const user = await User.create(data);
+
+      pubsub.publish(USER_ADDED, {
+        userAdded: user
+      });
+
+      return user;
+    },
     updateUser: (_, { id, data }) => User.findOneAndUpdate(id, data, { new: true }),
     deleteUser: async (_, { id}) => {
       const deleted = await User.findOneAndDelete(id);
       return Boolean(deleted);
     }
   },
+  Subscription: {
+    userAdded: {
+      subscribe: (obj, args, { pubsub }) => pubsub.asyncIterator(USER_ADDED),
+    }
+  }
 }
